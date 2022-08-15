44ca5184915e1f68b4dfd09d5e36d9d798348dd0
Edinson <edinson.ningles@gmail.com>
2022-08-15 20:28:39 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  data: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get('https://api.spacexdata.com/v4/rockets')
+      .subscribe((data: any) => {
+        this.data = data;
+      });
+  }
+}
